2f33fe4dc178cdd94ae1bc66dbe6eb726bd08847
Eliezer Steinbock <eliesteinbock@gmail.com>
2021-03-31 14:06:21 +0300

    Initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: [
+    '../src/components/**/*.stories.mdx',
+    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+}
