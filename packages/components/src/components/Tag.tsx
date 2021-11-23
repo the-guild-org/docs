8cfa565ae350101cf834bd50d17a4dfb489edb32
Dimitri POSTOLOV <dimitri.postolov@wing.eu>
2021-11-23 14:16:01 +0100

    fix TagsContainer when there are many tags (#251)

--- packages/components/src/components/Tag.tsx
+++ packages/components/src/components/Tag.tsx
@@ -1,7 +1,7 @@
 import tw, { styled } from 'twin.macro';
 
 export const Tag = styled.div(() => [
-  tw`bg-gray-200 inline border-0 rounded-md cursor-pointer outline-none font-default text-xs text-gray-500 px-2 py-1 mr-2`,
+  tw`bg-gray-200 inline border-0 rounded-md cursor-pointer outline-none font-default text-xs text-gray-500 px-2 py-1 mr-2 mb-2`,
 ]);
 
-export const TagsContainer = styled.div(() => [tw`py-2`]);
+export const TagsContainer = styled.div(() => [tw`py-2 flex flex-wrap`]);
