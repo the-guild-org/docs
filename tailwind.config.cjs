d60ce7928d9426488ceeae0fd2f3d6dec8b2cf34
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-08-08 16:23:14 +0200

    add tailwindcss-config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,51 +1,10 @@
-const colors = require('tailwindcss/colors');
-
-const makePrimaryColor =
-  l =>
-  ({ opacityValue }) => {
-    if (opacityValue === undefined) {
-      return `hsl(var(--nextra-primary-hue) 100% ${l}%)`;
-    }
-    return `hsl(var(--nextra-primary-hue) 100% ${l}% / ${opacityValue})`;
-  };
+const config = require('@theguild/tailwind-config')
 
 module.exports = {
-  darkMode: 'class',
+  ...config,
   content: ['./packages/components/src/**/*.{ts,tsx}'],
   theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
-      },
-    },
-    colors: {
-      transparent: 'transparent',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.neutral,
-      red: colors.red,
-      cyan: colors.cyan,
-      neutral: colors.neutral,
-      primary: {
-        50: makePrimaryColor(97),
-        100: makePrimaryColor(94),
-        200: makePrimaryColor(86),
-        300: makePrimaryColor(77),
-        400: makePrimaryColor(66),
-        500: makePrimaryColor(50),
-        600: makePrimaryColor(45),
-        700: makePrimaryColor(39),
-        750: makePrimaryColor(35),
-        800: makePrimaryColor(32),
-        900: makePrimaryColor(24),
-        1000: makePrimaryColor(12),
-      },
-    },
+    ...config.theme,
     extend: {
       animation: {
         gradient: 'gradient 15s ease infinite',
