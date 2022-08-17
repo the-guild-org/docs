3ef444761c7f3cbe8c0013dceba69da40e31cdec
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-08-17 17:58:49 +0200

    fix

--- packages/components/src/components/hero-gradient.tsx
+++ packages/components/src/components/hero-gradient.tsx
@@ -25,52 +25,49 @@ export const HeroGradient = ({
   className,
 }: IHeroGradientProps): ReactElement => {
   return (
-  <section className={clsx('overflow-hidden bg-white dark:bg-[#111] md:pt-14', className)}>
-    <div className="container relative max-w-[90rem]">
-      <div
-        className="absolute inset-0 overflow-hidden bg-black dark:bg-[#111] md:mx-6 md:rounded-3xl"
-        style={{ '--colorA': colors[0], '--colorB': colors[1] }}
-      >
-        <Shadow className="-top-24 -left-10 [background:var(--colorA)]" />
-        <Shadow className="right-[-450px] -top-5 hidden [background:var(--colorA)] md:block" />
-        <Shadow className="right-[-350px] bottom-[-500px] [background:var(--colorB)]" />
-        <Shadow className="-left-12 bottom-[-600px] [background:var(--colorB)]" />
-      </div>
-      <div className={clsx('px-6 md:px-14 md:pt-24 md:pb-14', image ? 'mb-32 pt-8 pb-72 md:mb-4' : 'py-20')}>
-        <div className="relative z-[1]">
-          <h1 className="mb-2.5 max-w-lg text-2xl font-bold text-white md:text-3xl">{title}</h1>
-          <p className="max-w-md text-base text-white opacity-70 md:text-lg">{description}</p>
+    <section className={clsx('overflow-hidden bg-white dark:bg-[#111] md:pt-14', className)}>
+      <div className="container relative max-w-[90rem]">
+        <div
+          className="absolute inset-0 overflow-hidden bg-black dark:bg-[#111] md:mx-6 md:rounded-3xl"
+          style={{ '--colorA': colors[0], '--colorB': colors[1] }}
+        >
+          <Shadow className="-top-24 -left-10 [background:var(--colorA)]" />
+          <Shadow className="right-[-450px] -top-5 hidden [background:var(--colorA)] md:block" />
+          <Shadow className="right-[-350px] bottom-[-500px] [background:var(--colorB)]" />
+          <Shadow className="-left-12 bottom-[-600px] [background:var(--colorB)]" />
         </div>
-        <div className="relative z-[1] mt-4 flex items-center gap-x-3 text-xs md:mt-9">
-          {link &&
-            toArray(link).map(link => (
-              <Button key={link.href} variant="secondary" {...link} className={clsx('!px-8', link.className)} />
-            ))}
-          {version && isValidElement(version) ? version : <span className="text-gray-50 opacity-60">{version}</span>}
-        </div>
-        {image &&
-          (isValidElement(image) ? (
-            image
-          ) : (
-            <img
-              src={image.src}
-              alt={image.alt}
-              className="
-                absolute
-                -bottom-36
-                -right-4
-                w-full
-                max-w-sm
-                sm:max-w-md
-                md:-top-14
-                md:-right-16
-              "
-              {...restProps.imageProps}
+        <div className={clsx('px-6 md:px-14 md:pt-24 md:pb-14', image ? 'mb-32 pt-8 pb-72 md:mb-4' : 'py-20')}>
+          <div className="relative z-[1]">
+            <h1 className="mb-2.5 max-w-lg text-2xl font-bold text-white md:text-3xl">{title}</h1>
+            <p className="max-w-md text-base text-white opacity-70 md:text-lg">{description}</p>
+          </div>
+          <div className="relative z-[1] mt-4 flex items-center gap-x-3 text-xs md:mt-9">
+            {link &&
+              toArray(link).map(link => (
+                <Button key={link.href} variant="secondary" {...link} className={clsx('!px-8', link.className)} />
+              ))}
+            {version && isValidElement(version) ? version : <span className="text-gray-50 opacity-60">{version}</span>}
+          </div>
+          {image && (
+            <NextImage
+              {...image}
+              className={clsx(
+                `
+                  absolute
+                  -bottom-36
+                  -right-4
+                  w-full
+                  max-w-sm
+                  sm:max-w-md
+                  md:-top-14
+                  md:-right-16`,
+                image.className
+              )}
             />
-          ))}
+          )}
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
   );
 };
 
