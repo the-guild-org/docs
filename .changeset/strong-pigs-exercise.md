61541e2d672772a14d56dba0757999dea35e6383
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-09-21 14:45:16 +0300

    enhance `withGuildDocs` (#444)
    * enhance `withGuildDocs`, add `nextConfig.basePath`, `nextConfig.webpack` with `applyUnderscoreRedirects`, `images.unoptimized = true` if `basePath` was specified
    
    * fixes tiny-lru
    
    * add critters to dependencies
    
    * chore(dependencies): updated changesets for modified dependencies
    
    * Update packages/components/package.json
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ .changeset/strong-pigs-exercise.md
@@ -0,0 +1,5 @@
+---
+'@theguild/components': patch
+---
+
+enhance `withGuildDocs`, add `nextConfig.basePath`, `nextConfig.webpack` with `applyUnderscoreRedirects`, `images.unoptimized = true` if `basePath` was specified
--- packages/components/src/next.config.ts
+++ packages/components/src/next.config.ts
@@ -2,13 +2,20 @@ import { NextConfig } from 'next';
 import nextBundleAnalyzer from '@next/bundle-analyzer';
 import nextra from 'nextra';
 import withVideos from 'next-videos';
-import { remarkMermaid } from './remark-mermaid.js';
 import remarkMdxDisableExplicitJsx from 'remark-mdx-disable-explicit-jsx';
+import { remarkMermaid } from './remark-mermaid.js';
+import { applyUnderscoreRedirects } from './underscore-redirects.js';
 
 export const withGuildDocs = ({
   themeConfig = './theme.config.tsx',
   ...nextConfig
 }: NextConfig & { themeConfig?: string } = {}) => {
+  if ('webpack' in nextConfig) {
+    throw new Error(
+      '`nextConfig.webpack` is already specified, remove it to avoid overwrite `applyUnderscoreRedirects` setting'
+    );
+  }
+
   const withBundleAnalyzer = nextBundleAnalyzer({
     enabled: process.env.ANALYZE === 'true',
   });
@@ -20,32 +27,39 @@ export const withGuildDocs = ({
     mdxOptions: {
       remarkPlugins: [
         [
-          //
+          // replace <iframe />, <video />, <source /> tags in MDX
           remarkMdxDisableExplicitJsx,
-          { whiteList: ['video', 'iframe', 'source'] },
+          { whiteList: ['iframe', 'video', 'source'] },
         ],
         remarkMermaid,
       ],
     },
   });
 
+  const basePath = process.env.NEXT_BASE_PATH;
+
   return withBundleAnalyzer(
     withVideos(
       withNextra({
         reactStrictMode: true,
         swcMinify: true,
+        basePath,
+        webpack(config, meta) {
+          applyUnderscoreRedirects(config, meta);
+          return config;
+        },
         ...nextConfig,
         experimental: {
           optimizeCss: true,
           newNextLinkBehavior: true,
-          // @ts-expect-error -- TODO: check next version in Nextra and remove this
-          images: {
-            allowFutureImage: true, // next/future/image
-            // @ts-expect-error -- TODO: check next version in Nextra and remove this
-            ...nextConfig.experimental?.images,
-          },
           ...nextConfig.experimental,
         },
+        ...(basePath && {
+          images: {
+            unoptimized: true, // doesn't work with `next export`,
+            ...nextConfig.images,
+          },
+        }),
       })
     )
   );
