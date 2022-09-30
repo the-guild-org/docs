0a1ac54841424e089906395fe8ac0decce7301a0
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-09-30 12:20:32 +0200

    update example website

--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -1,6 +1,1 @@
-const { content, ...config } = require('@theguild/tailwind-config');
-
-module.exports = {
-  ...config,
-  content: [...content, '../node_modules/@theguild/components/dist/**/*.{js,mjs}'],
-};
+module.exports = require('@theguild/tailwind-config');
