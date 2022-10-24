d6dec03a1bfb20b543fea772ed52c3a3c6e5e389
Denis Badurina <badurinadenis@gmail.com>
2022-10-24 15:22:04 +0200

    Themed mermaid and improvements (#517)
    * mermaid improvements and theming
    
    * changeset
    
    * add secondary colors
    
    * catch mermaid render errors

--- packages/components/src/mermaid.tsx
+++ packages/components/src/mermaid.tsx
@@ -1,29 +1,71 @@
-import { ReactElement, useEffect, useState } from 'react';
-import mermaid, { Config } from 'mermaid';
+import { ReactElement, useEffect, useId, useState } from 'react';
+import mermaid from 'mermaid';
 import { useTheme } from 'nextra-theme-docs';
 
-/**
- * Assign a unique ID to each mermaid svg as per requirements of `mermaid.render`.
- */
-let id = 0;
+let initialized = false;
 
 export const Mermaid = ({ chart }: { chart: string }): ReactElement => {
   const { resolvedTheme } = useTheme();
-  const [svg, setSVG] = useState('');
+  const id = useId();
+  const [svg, setSvg] = useState('');
 
+  // must use useEffect bacause mermaid needs the browser
   useEffect(() => {
-    mermaid.initialize({
-      startOnLoad: true,
-      theme: resolvedTheme as Config['theme'],
-      fontFamily: 'inherit',
+    // init is needed only once
+    if (!initialized) {
+      initialized = true;
+      mermaid.initialize({
+        fontFamily: 'inherit',
+      });
+    }
+
+    // see https://mermaid-js.github.io/mermaid/#/theming?id=theme-variables-reference-table
+    const theme = JSON.stringify({
+      theme: 'base',
+      themeVariables: {
+        fontSize: '1.125rem', // text-lg
+        lineColor: resolvedTheme === 'dark' ? 'white' : 'black',
+
+        primaryColor:
+          resolvedTheme === 'dark'
+            ? '#1e293b' // slate-800
+            : '#cbd5e1', // slate-300
+        primaryTextColor: resolvedTheme === 'dark' ? 'white' : 'black',
+        primaryBorderColor:
+          resolvedTheme === 'dark'
+            ? '#cbd5e1' // slate-300
+            : '#1e293b', // slate-800
+
+        secondaryColor:
+          resolvedTheme === 'dark'
+            ? '#475569' // slate-600
+            : '#f1f5f9', // slate-100
+        secondaryBorderColor:
+          resolvedTheme === 'dark'
+            ? '#f1f5f9' // slate-100
+            : '#475569', // slate-600
+
+        tertiaryColor:
+          resolvedTheme === 'dark'
+            ? '#475569' // slate-600
+            : '#f1f5f9', // slate-100
+        tertiaryBorderColor:
+          resolvedTheme === 'dark'
+            ? '#f1f5f9' // slate-100
+            : '#475569', // slate-600
+      },
     });
+
     try {
-      const renderedSvg = mermaid.render(`mermaid-svg-${(id += 1)}`, chart);
-      setSVG(renderedSvg);
+      const svg = mermaid.render(
+        id.replace(/[^a-zA-Z]+/g, ''), // strip special chars from useId
+        `%%{init:${theme}}%%\n${chart}` // apply theme and supply chart
+      );
+      setSvg(svg);
     } catch (error) {
       console.error('Error while rendering mermaid', error);
     }
-  }, [resolvedTheme, chart]);
+  }, [resolvedTheme, id, chart]);
 
   return <div className="mt-6 flex justify-center" dangerouslySetInnerHTML={{ __html: svg }} />;
 };
