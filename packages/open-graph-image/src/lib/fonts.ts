e4715823546bdc9467692d544dd8a3598d0c6c49
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-11-07 13:01:22 +0100

    prettify

--- packages/open-graph-image/src/lib/fonts.ts
+++ packages/open-graph-image/src/lib/fonts.ts
@@ -1,12 +1,4 @@
-export async function loadGoogleFont({
-  family,
-  weight,
-  text,
-}: {
-  family: string;
-  weight?: number;
-  text?: string;
-}) {
+export async function loadGoogleFont({ family, weight, text }: { family: string; weight?: number; text?: string }) {
   const params: Record<string, string> = {
     family: `${encodeURIComponent(family)}${weight ? `:wght@${weight}` : ''}`,
   };
@@ -18,7 +10,7 @@ export async function loadGoogleFont({
   }
 
   const url = `https://fonts.googleapis.com/css2?${Object.keys(params)
-    .map((key) => `${key}=${params[key]}`)
+    .map(key => `${key}=${params[key]}`)
     .join('&')}`;
 
   const css = await fetch(`${url}`, {
@@ -27,16 +19,14 @@ export async function loadGoogleFont({
       'User-Agent':
         'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1',
     },
-  }).then((res) => res.text());
+  }).then(res => res.text());
 
   // Get the font URL from the CSS text
-  const fontUrl = css.match(
-    /src: url\((.+)\) format\('(opentype|truetype)'\)/
-  )?.[1];
+  const fontUrl = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/)?.[1];
 
   if (!fontUrl) {
     throw new Error('Could not find font URL');
   }
 
-  return fetch(fontUrl).then((res) => res.arrayBuffer());
+  return fetch(fontUrl).then(res => res.arrayBuffer());
 }
