08be06ff97826975c7603a2a8e9704dbfb3f163b
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-01-06 01:34:57 +0100

    remove already enabled rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,27 +11,19 @@ module.exports = {
     'plugin:storybook/recommended',
   ],
   rules: {
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off', // import of react no longer required
     'tailwindcss/classnames-order': 'off', // conflicts with official prettier-plugin-tailwindcss and tailwind v3
     // set more strict to highlight in editor
     'tailwindcss/enforces-shorthand': 'error',
     'tailwindcss/migration-from-tailwind-2': 'error',
+    'tailwindcss/no-custom-classname': 'warn',
     'no-unused-expressions': 'error',
-    'react/self-closing-comp': 'error',
-    'no-implicit-coercion': 'error',
-    'react/no-unescaped-entities': 'off',
     'no-var': 'error',
-    'object-shorthand': ['error', 'always'],
-    'prefer-const': 'error',
     'prefer-destructuring': ['error', { object: true }],
     'prefer-template': 'error',
+    'react/prop-types': 'off',
     'react/jsx-boolean-value': ['error', 'never'],
-    'react/jsx-curly-brace-presence': ['error', 'never'],
-    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
     '@typescript-eslint/array-type': ['error', { readonly: 'generic' }],
     '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'no-type-imports' }],
-    'tailwindcss/no-custom-classname': 'warn',
   },
   settings: {
     tailwindcss: {
