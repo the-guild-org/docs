3b34348db8b91fab182c125b3180252886722674
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-01-12 19:35:41 +0100

    fix lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,13 +14,9 @@ module.exports = {
     // set more strict to highlight in editor
     'tailwindcss/enforces-shorthand': 'error',
     'tailwindcss/migration-from-tailwind-2': 'error',
-    'tailwindcss/no-custom-classname': 'warn',
-    'no-unused-expressions': 'error',
-    'no-var': 'error',
+    'tailwindcss/no-custom-classname': 'error',
     'prefer-destructuring': ['error', { object: true }],
     'prefer-template': 'error',
-    'react/prop-types': 'off',
-    'react/jsx-boolean-value': ['error', 'never'],
     '@typescript-eslint/array-type': ['error', { readonly: 'generic' }],
     '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'no-type-imports' }],
 
@@ -28,20 +24,10 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'warn',
     'jsx-a11y/click-events-have-key-events': 'warn',
     'jsx-a11y/no-static-element-interactions': 'warn',
-    'import/extensions': 'warn',
   },
   settings: {
     tailwindcss: {
       config: 'tailwind.config.cjs',
-      whitelist: [
-        'aa-ItemLink',
-        'aa-ItemContent',
-        'aa-ItemContentBody',
-        'aa-ItemContentTitle',
-        'aa-ItemContentSubtitle',
-        'aa-SourceHeader',
-        'aa-SourceHeaderTitle',
-      ],
     },
   },
   overrides: [
