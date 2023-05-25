0bb670b150fb06cb6011ecc945f5065e610cb6a3
beerose <alexsandra.sikora@gmail.com>
2023-05-25 12:32:33 +0100

    Improve marketplace search a bit

--- packages/components/src/components/tag.tsx
+++ packages/components/src/components/tag.tsx
@@ -2,14 +2,18 @@ import { ReactElement, ReactNode } from 'react';
 
 export const Tag = ({
   children,
+  selected,
   onClick,
 }: {
   children: ReactNode;
+  selected?: boolean;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }) => {
   return (
     <button
-      className="mr-2 mb-2 inline cursor-pointer rounded-md border-0 bg-gray-200 px-2 py-1 text-xs text-gray-500 outline-none"
+      className={`mr-2 mb-2 inline cursor-pointer rounded-md border-0 ${
+        selected ? 'bg-gray-400 text-gray-700' : 'bg-gray-200 text-gray-500'
+      } px-2 py-1 text-xs text-gray-500 outline-none`}
       onClick={onClick}
     >
       {children}
