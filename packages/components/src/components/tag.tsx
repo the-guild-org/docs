1611f82951a3cbab2f6ef31264d807a9c4896ccc
beerose <alexsandra.sikora@gmail.com>
2023-05-25 16:18:42 +0100

    Try to change tag.tsx

--- packages/components/src/components/tag.tsx
+++ packages/components/src/components/tag.tsx
@@ -11,9 +11,9 @@ export const Tag = ({
 }) => {
   return (
     <button
-      className={`mr-2 mb-2 inline cursor-pointer rounded-md border-0 ${
+      className={`mr-2 mb-2 inline cursor-pointer rounded-md border-0 px-2 py-1 text-xs text-gray-500 outline-none ${
         selected ? 'bg-gray-400 text-gray-700' : 'bg-gray-200 text-gray-500'
-      } px-2 py-1 text-xs text-gray-500 outline-none`}
+      }`}
       onClick={onClick}
     >
       {children}
