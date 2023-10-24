fc53b9756c10323cadb4e892adf033d608f53f82
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-23 19:48:04 -0500

    all in one

--- packages/components/src/compile.ts
+++ packages/components/src/compile.ts
@@ -1,4 +1,4 @@
 export * from 'nextra/compile';
-export { remarkLinkRewrite } from 'nextra/mdx-plugins';
+export { remarkLinkRewrite } from 'nextra/remark-plugins';
 export { buildDynamicMDX, buildDynamicMeta } from 'nextra/remote';
 export { fetchFilePathsFromGitHub } from 'nextra/fetch-filepaths-from-github';
