aa6eb053c28421ab2b6d79de89ebaad86efafce5
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-04-08 17:10:24 +0200

    upd

--- website/postcss.config.ts
+++ website/postcss.config.ts
@@ -1,8 +1,8 @@
 export default {
   plugins: {
-    "postcss-import": {},
+    'postcss-import': {},
     tailwindcss: {},
     autoprefixer: {},
-    ...process.env.NODE_ENV === "production" && { cssnano: {} }
+    ...process.env.NODE_ENV === 'production' && { cssnano: {} }
   }
 }
