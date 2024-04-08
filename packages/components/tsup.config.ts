1bc62efa43a4f3e3d007d9cb399e086100b48df3
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-04-08 12:58:27 +0200

    upd deps

--- packages/components/tsup.config.ts
+++ packages/components/tsup.config.ts
@@ -23,7 +23,6 @@ export default defineConfig([
     outExtension: () => ({ js: '.js' }),
     external: ['semver'],
     esbuildPlugins: [
-      // @ts-expect-error fixme
       svgr({
         exportType: 'named',
         typescript: true,
