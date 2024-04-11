bb2b32cf949a3b4cdff1ad536a7861be01300517
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-04-11 17:14:41 +0200

    aa

--- packages/components/src/types/components.ts
+++ packages/components/src/types/components.ts
@@ -16,31 +16,11 @@ export type ILink = Omit<LinkProps, 'href'> &
     sameSite?: boolean;
   };
 
-export type IHeaderLink = ILink & {
-  label: string;
-  menu?: ReactNode;
-};
-
 export interface IFooterExtendedProps {
   className?: string;
   sameSite?: boolean;
   resources?: ILink[];
   logo?: ILink;
-}
-
-export interface ISearchBarProps {
-  version?: 'v1' | 'v2';
-  algolia?: {
-    appId: string;
-    searchApiKey: string;
-    indexName: string;
-  };
-  accentColor: string;
-  title: string;
-  placeholder: string;
-  isFull?: boolean;
-  onHandleModal?: (state: boolean) => void;
-  className?: string;
 }
 
 export interface IFeatureListProps {
