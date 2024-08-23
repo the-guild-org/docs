a500007fb190a56ec991d4cba6f4976e70421b30
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-08-23 15:07:20 +0200

    Promote Heading component

---
+++ packages/components/src/components/heading.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Heading as _Heading, HeadingProps } from './heading';
+
+export default {
+  title: 'Components/Heading',
+  component: _Heading,
+  args: {
+    children: 'Open-source GraphQL management platform',
+  },
+  argTypes: {
+    as: {
+      control: { type: 'select' },
+      options: ['h1', 'h2', 'h3', 'div'],
+    },
+    size: {
+      control: { type: 'select' },
+      options: ['xl', 'lg', 'md', 'sm'],
+    },
+    children: {
+      control: 'text',
+    },
+  },
+} satisfies Meta<HeadingProps>;
+
+export const Heading: StoryObj<HeadingProps> = {
+  args: {
+    as: 'h1',
+    size: 'xl',
+  },
+};
