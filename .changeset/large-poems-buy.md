da325f4501e4f13a0f3e463360f5eb28bc25ad0c
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-08-31 23:57:54 +0200

    da

---
+++ .changeset/large-poems-buy.md
@@ -0,0 +1,5 @@
+---
+'@theguild/remark-mermaid': patch
+---
+
+should correctly escape '`' if code block ends invalid
---
+++ packages/remark-mermaid/src/index.test.ts
@@ -0,0 +1,75 @@
+import { compileMdx } from 'nextra/compile';
+
+describe('remarkMermaid', () => {
+  it('should parse', async () => {
+    const doc = `\`\`\`mermaid
+graph TD
+A
+\`\`\``;
+
+    const { result } = await compileMdx(doc);
+    expect(
+      result.slice(result.indexOf('function _createMdxContent'), result.indexOf('return {') - 1),
+    ).toMatchInlineSnapshot(`
+      "function _createMdxContent(props) {
+        const {Mermaid} = {
+          ..._provideComponents(),
+          ...props.components
+        };
+        if (!Mermaid) _missingMdxReference("Mermaid", true);
+        return _jsx(Mermaid, {
+          chart: \`graph TD
+      A\`
+        });
+      }"
+    `);
+  });
+  it('should parse invalid ending ``', async () => {
+    const doc = `\`\`\`mermaid
+graph TD
+A
+\`\``;
+
+    const { result } = await compileMdx(doc);
+    expect(
+      result.slice(result.indexOf('function _createMdxContent'), result.indexOf('return {') - 1),
+    ).toMatchInlineSnapshot(`
+      "function _createMdxContent(props) {
+        const {Mermaid} = {
+          ..._provideComponents(),
+          ...props.components
+        };
+        if (!Mermaid) _missingMdxReference("Mermaid", true);
+        return _jsx(Mermaid, {
+          chart: \`graph TD
+      A
+      \`\`\`
+        });
+      }"
+    `);
+  });
+  it('should parse invalid ending `', async () => {
+    const doc = `\`\`\`mermaid
+graph TD
+A
+\``;
+
+    const { result } = await compileMdx(doc);
+    expect(
+      result.slice(result.indexOf('function _createMdxContent'), result.indexOf('return {') - 1),
+    ).toMatchInlineSnapshot(`
+      "function _createMdxContent(props) {
+        const {Mermaid} = {
+          ..._provideComponents(),
+          ...props.components
+        };
+        if (!Mermaid) _missingMdxReference("Mermaid", true);
+        return _jsx(Mermaid, {
+          chart: \`graph TD
+      A
+      \`\`
+        });
+      }"
+    `);
+  });
+});
