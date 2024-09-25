c389a8e9a7df47cadd44e6eeb2c1396bfe6a6f31
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-09-25 17:37:14 +0200

    Lower the margin to acknowledge the new 1px border

--- packages/components/src/components/hive-navigation/index.tsx
+++ packages/components/src/components/hive-navigation/index.tsx
@@ -86,7 +86,7 @@ export function HiveNavigation({
       ref={containerRef}
       data-scrolled="false"
       className={cn(
-        'sticky top-0 z-20 border-b border-beige-400 bg-[rgb(var(--nextra-bg))] px-6 py-4 text-green-1000 transition-[border-color] duration-500 data-[scrolled=false]:border-transparent md:my-2 dark:border-neutral-700 dark:text-neutral-200 [&.light]:bg-white [&.light]:text-green-1000',
+        'sticky top-0 z-20 border-b border-beige-400 bg-[rgb(var(--nextra-bg))] px-6 py-4 text-green-1000 transition-[border-color] duration-500 data-[scrolled=false]:border-transparent md:mb-[7px] md:mt-2 dark:border-neutral-700 dark:text-neutral-200 [&.light]:bg-white [&.light]:text-green-1000',
         className?.includes('light') && 'light',
       )}
     >
