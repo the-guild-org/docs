a9d55b623cea755f72df880def0bfd0cb21af80f
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-09-26 23:27:02 +0200

    more

--- packages/components/src/env.d.ts
+++ packages/components/src/env.d.ts
@@ -13,7 +13,7 @@ declare module 'next-videos' {
 }
 
 declare module '*.svg' {
-  import type { FC, SVGProps } from 'react'
-  export const ReactComponent: FC<SVGProps<SVGElement>>
+  import type { FC, SVGProps } from 'react';
+  export const ReactComponent: FC<SVGProps<SVGElement>>;
 }
 declare module '*.png';
