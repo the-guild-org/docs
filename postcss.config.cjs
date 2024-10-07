82f45b818bd2a2f574e92091bd023b3d9e7db475
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-10-07 19:36:34 +0200

    Add an absolute path to tailwind.config.ts

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,1 +1,14 @@
-module.exports = require('@theguild/tailwind-config/postcss.config');
+const path = require('node:path');
+
+const base = require('@theguild/tailwind-config/postcss.config');
+
+module.exports = {
+  ...base,
+  plugins: {
+    ...base.plugins,
+    tailwindcss: {
+      // Without an absolute path here, `tsup` building components cannot find the config.
+      config: path.join(__dirname, 'tailwind.config.ts'),
+    },
+  },
+};
