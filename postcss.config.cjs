afd94bd760dd1dd020bf8ff23289fb5ab61c48eb
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-10-07 19:56:19 +0200

    Revert "Add an absolute path to tailwind.config.ts"
    This reverts commit 82f45b818bd2a2f574e92091bd023b3d9e7db475.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,14 +1,1 @@
-const path = require('node:path');
-
-const base = require('@theguild/tailwind-config/postcss.config');
-
-module.exports = {
-  ...base,
-  plugins: {
-    ...base.plugins,
-    tailwindcss: {
-      // Without an absolute path here, `tsup` building components cannot find the config.
-      config: path.join(__dirname, 'tailwind.config.ts'),
-    },
-  },
-};
+module.exports = require('@theguild/tailwind-config/postcss.config');
