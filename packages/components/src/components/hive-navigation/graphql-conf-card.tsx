c366f3f2490fd77cd13da4c62047652f830a5776
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-10-18 20:49:20 +0200

    Update GraphQLConfCard to link to YT (#1736)
    Co-authored-by: Dimitri POSTOLOV <dmytropostolov@gmail.com>

--- packages/components/src/components/hive-navigation/graphql-conf-card.tsx
+++ packages/components/src/components/hive-navigation/graphql-conf-card.tsx
@@ -8,19 +8,23 @@ export interface GraphQLConfCardProps {
 }
 export function GraphQLConfCard({ image }: GraphQLConfCardProps) {
   return (
-    <NavigationMenuLink href="https://graphql.org/conf/2024/" className="group w-[358px]">
+    <NavigationMenuLink
+      href="https://www.youtube.com/playlist?list=PL43V96KpNj7OMvmfL0WFKP6LpoboM8Hde"
+      className="group w-[358px]"
+    >
       <Image alt="" {...image} width={358} height={200} />
       <strong className="mt-6 block text-xl font-medium leading-7 text-green-1000 dark:text-neutral-100">
         GraphQLConf 2024
       </strong>
       <p className="mt-4 text-sm font-medium leading-5 text-green-800 dark:text-neutral-200">
-        September 10-12 | San Fransico CA
+        September 10-12 | San Francisco CA
       </p>
       <p className="mt-2 text-sm font-normal leading-5 text-green-800 dark:text-neutral-200">
         The official GraphQL conference hosted by GraphQL Foundation.
       </p>
-      <span className="-mx-2 mt-4 flex items-center gap-2 rounded-lg p-2 font-medium text-green-800 transition-colors group-hover:text-green-1000 dark:text-neutral-200 dark:group-hover:text-neutral-100">
-        <span>Meet us at GraphQLConf 2024</span> <ArrowIcon />
+      <span className="mt-6 flex items-center gap-2 rounded-lg font-medium text-green-800 transition-colors group-hover:text-green-1000 dark:text-neutral-200 dark:group-hover:text-neutral-100">
+        Watch The Guild at GraphQLConf 2024
+        <ArrowIcon />
       </span>
     </NavigationMenuLink>
   );
