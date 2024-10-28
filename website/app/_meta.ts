68ca2b6b1c46be5ebeb80b00474606c94e1c732d
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-10-28 20:48:31 +0300

    more

--- website/app/_meta.ts
+++ website/app/_meta.ts
@@ -1,1 +1,27 @@
-export default {}
+export default {
+  index: {
+    title: 'Home',
+    type: 'page',
+    display: 'hidden',
+    theme: {
+      layout: 'raw',
+    },
+  },
+  docs: {
+    title: 'Documentation',
+    type: 'page',
+  },
+  'contact-us-link': {
+    title: 'Contact Us',
+    type: 'page',
+    href: 'https://the-guild.dev/contact',
+    newWindow: true,
+  },
+  '404': {
+    type: 'page',
+    theme: {
+      timestamp: false,
+      typesetting: 'article',
+    },
+  },
+};
