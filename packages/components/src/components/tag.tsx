e8a01f7a71ebf35e6b92862e71550a8cac5cfc87
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-10-28 16:13:48 +0100

    Add one more hive-focus style

--- packages/components/src/components/tag.tsx
+++ packages/components/src/components/tag.tsx
@@ -14,7 +14,7 @@ export const Tag = ({
     <button
       tabIndex={onClick ? 0 : -1}
       className={cn(
-        'inline cursor-pointer rounded-full border-0 px-3 py-1 text-xs font-medium outline-none',
+        'hive-focus inline cursor-pointer rounded-full border-0 px-3 py-1 text-xs font-medium outline-none',
         selected
           ? 'bg-neutral-700 text-white [.green_&]:bg-green-600'
           : 'bg-neutral-900/5 text-neutral-800 dark:bg-neutral-200/10 dark:text-neutral-200 [.green_&]:bg-green-700 [.green_&]:text-green-200',
