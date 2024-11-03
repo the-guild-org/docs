2944b3d8c80d6268b421ec266487104c7cfa5a1e
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-03 12:08:08 +0300

    more

--- packages/components/src/mdx-components.tsx
+++ packages/components/src/mdx-components.tsx
@@ -1,12 +1,27 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
 import clsx from 'clsx';
 import { useMDXComponents as getDocsMDXComponents } from 'nextra-theme-docs';
 
 const docsComponents = getDocsMDXComponents({
-  source({ src, type, ...props }) {
+  async source({ src, type, ...props }) {
     if (!src) {
       throw new Error('Must provide `src` prop');
     }
-    let ext = src.replace(/.*\./, '');
+    if (src.startsWith('/')) {
+      const filePath = path.join(process.cwd(), 'public', src);
+      try {
+        await fs.access(filePath);
+      } catch (error) {
+        const relativePath = path.relative(process.cwd(), filePath);
+        if ((error as any).code === 'ENOENT') {
+          throw new Error(`File doesn't exist: ${relativePath}`);
+        }
+        throw new Error(`Error checking file: ${relativePath}`);
+      }
+    }
+
+    let ext = path.extname(src).slice(1); // remove dot;
     if (ext === 'mov') {
       ext = 'quicktime';
     }
--- website/postcss.config.js
+++ website/postcss.config.js
@@ -1,2 +1,1 @@
-// eslint-disable-next-line import/no-default-export -- false positive
 export { default } from '@theguild/tailwind-config/postcss.config';
