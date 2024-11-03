3a0832b82b7d7bcbf54705cfd8fc6462fa558650
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-03 12:22:33 +0300

    more

--- packages/components/src/env.d.ts
+++ packages/components/src/env.d.ts
@@ -1,17 +1,3 @@
-declare module '@next/bundle-analyzer' {
-  import { NextConfig } from 'next';
-
-  export default function nextBundleAnalyzer(config: {
-    enabled: boolean;
-  }): (nextConfig: NextConfig) => NextConfig;
-}
-
-declare module 'next-videos' {
-  import { NextConfig } from 'next';
-
-  export default function nextVideos(nextConfig: NextConfig): NextConfig;
-}
-
 declare module '*.svg' {
   import { FC, SVGProps } from 'react';
   export const ReactComponent: FC<SVGProps<SVGElement>>;
