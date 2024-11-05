0d59f313e5bad92cd658a841fb8f294a42d100e0
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-05 23:17:58 +0300

    simplify tsup

--- packages/components/tsup.config.ts
+++ packages/components/tsup.config.ts
@@ -1,81 +1,70 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
 import svgr from 'esbuild-plugin-svgr';
-import { defineConfig, Options } from 'tsup';
+import { defineConfig } from 'tsup';
 
-const options: Options = {
+export default defineConfig({
+  name: 'components',
   format: 'esm',
-  target: 'es2021',
   dts: true,
+  bundle: false,
   outExtension: () => ({ js: '.js' }),
-};
-
-const PATH_WITH_SVG = [
-  'src/logos/index.tsx',
-  'src/components/icons/index.ts',
-  'src/components/decorations/index.tsx',
-];
-
-export default defineConfig([
-  {
-    ...options,
-    name: 'components',
-    entry: [
-      'src/**/*.{ts,tsx}',
-      'src/static/illustrations/*.png',
-      '!**/*.stories.{ts,tsx}',
-      '!src/helpers/dummy.ts',
-      '!src/*.d.ts',
-      ...PATH_WITH_SVG.map(filePath => `!${filePath}`),
-    ],
-    bundle: false,
-    loader: {
-      '.png': 'copy',
-    },
-    async onSuccess() {
-      const serverPackageJSON = path.join(process.cwd(), 'dist', 'server', 'package.json');
-      await fs.writeFile(serverPackageJSON, '{"type":"module","sideEffects":false}');
-    },
-    plugins: [
-      {
-        // Strip `node:` prefix from imports because
-        // Next.js only polyfills `path` and not `node:path` for browser
-        name: 'strip-node-colon',
-        renderChunk(code) {
-          const replaced = code.replaceAll(/ from "node:(?<moduleName>.*?)";/g, matched =>
-            matched.replace('node:', ''),
-          );
-          return { code: replaced };
+  entry: [
+    'src/**/*.{ts,tsx}',
+    'src/static/illustrations/*.png',
+    'src/**/*.svg',
+    '!**/*.stories.{ts,tsx}',
+    '!src/helpers/dummy.ts',
+    '!src/*.d.ts',
+  ],
+  loader: {
+    '.png': 'copy',
+  },
+  async onSuccess() {
+    const serverPackageJSON = path.join(process.cwd(), 'dist', 'server', 'package.json');
+    await fs.writeFile(serverPackageJSON, '{"type":"module","sideEffects":false}');
+  },
+  esbuildPlugins: [
+    svgr({
+      exportType: 'named',
+      typescript: true,
+      jsx: {
+        // svgo's removeXMLNS plugin doesn't work for some reason...
+        babelConfig: {
+          plugins: [
+            [
+              '@svgr/babel-plugin-remove-jsx-attribute',
+              { elements: ['svg'], attributes: ['xmlns'] },
+            ],
+          ],
         },
       },
-    ],
-  },
-  // We need to bundle svg files separately, due required `bundle: true` option which is set by default
-  {
-    ...options,
-    name: 'components/icons',
-    entry: Object.fromEntries(
-      PATH_WITH_SVG.map(filePath => [
-        filePath.replace('src/', '').replace(/\.tsx?$/, ''),
-        filePath,
-      ]),
-    ),
-    esbuildPlugins: [
-      svgr({
-        exportType: 'named',
-        typescript: true,
-        jsx: {
-          // svgo's removeXMLNS plugin doesn't work for some reason...
-          babelConfig: {
-            plugins: [
-              [
-                '@svgr/babel-plugin-remove-jsx-attribute',
-                { elements: ['svg'], attributes: ['xmlns'] },
-              ],
-            ],
-          },
-        },
-      }),
-    ],
-  },
-]);
+    }),
+  ],
+  plugins: [
+    {
+      // Strip `node:` prefix from imports
+      // Next.js only polyfills `path` and not `node:path` for browser
+      name: 'strip-node-colon',
+      renderChunk(code) {
+        // (?<= from ")
+        // Positive lookbehind asserts that the pattern we're trying to match is preceded by
+        // ` from "`, but does not include ` from "` in the actual match.
+        //
+        // (?=";)
+        // Positive lookahead asserts that the pattern is followed by `";`, but does not include
+        // `";` in the match.
+        const replaced = code.replaceAll(/(?<= from ")node:(.+)(?=";)/g, '$1');
+        return { code: replaced };
+      },
+    },
+    {
+      // Strip `.svg` suffix from imports
+      name: 'strip-dot-svg',
+      renderChunk(code) {
+        const replaced = code.replaceAll(/(?<= from ")(.+)\.svg(?=";)/g, '$1');
+        return { code: replaced };
+      },
+    },
+  ],
+});
