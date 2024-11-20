711364e1667ba47dee59434f21be9be7acdc77b2
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-11-20 20:54:58 +0100

    Adds OSS Friends link to the footer, but only on /graphql/hive pages

---
+++ .changeset/beige-cheetahs-hang.md
@@ -0,0 +1,5 @@
+---
+'@theguild/components': minor
+---
+
+Adds OSS Friends link to the footer, but only on /graphql/hive pages
--- packages/components/src/components/hive-footer.tsx
+++ packages/components/src/components/hive-footer.tsx
@@ -15,6 +15,7 @@ import {
 
 export interface HiveFooterProps extends IFooterExtendedProps {
   description?: string;
+  renderedInHive?: boolean;
 }
 
 export function HiveFooter({
@@ -23,6 +24,7 @@ export function HiveFooter({
   resources = [],
   sameSite,
   description,
+  renderedInHive = false,
 }: HiveFooterProps) {
   description ||= 'Open-source GraphQL management platform';
 
@@ -49,6 +51,12 @@ export function HiveFooter({
           </div>
           <div className="flex flex-col gap-[inherit]">
             <List heading="Company" links={COMPANY} />
+            {renderedInHive ? <a
+              href="https://the-guild.dev/graphql/hive/oss-friends"
+              className="hive-focus -m-2 rounded p-2 font-medium hover:text-blue-700 hover:underline dark:hover:text-blue-100"
+            >
+              OSS Friends
+            </a> : null}
             <a
               href="https://the-guild.dev/graphql/hive#pricing"
               className="hive-focus -m-2 rounded p-2 font-medium hover:text-blue-700 hover:underline dark:hover:text-blue-100"
