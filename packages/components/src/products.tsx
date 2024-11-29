483b8fec5e28952b5bc1f28b2dc5c7fa0816646f
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-11-29 18:39:38 +0100

    Update PRODUCTS_MENU_LIST to work with new Nextra (without .type)

--- packages/components/src/products.tsx
+++ packages/components/src/products.tsx
@@ -253,53 +253,46 @@ export const SIX_HIGHLIGHTED_PRODUCTS = [
 ];
 
 /** List of products displayed in hamburger menu. */
-export const PRODUCTS_MENU_LIST: Record<string, { type: 'separator'; title: ReactNode }> =
-  Object.fromEntries(
-    (
-      [
-        'The GraphQL Stack',
-        ...FOUR_MAIN_PRODUCTS,
-        'Libraries',
-        ...SIX_HIGHLIGHTED_PRODUCTS,
-      ] as const
-    ).map((item, i) => {
-      if (typeof item === 'string') {
-        return [
-          i,
-          {
-            type: 'separator',
-            title: (
-              <>
-                {/* This is a one-off class. The margins and paddings of the parent list item are were large. */}
-                {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
-                <style className="hive-label-separator">
-                  {'li:has(>.hive-label-separator) { margin: 0.75rem 0 0.25rem 0; padding: 0 }'}
-                </style>
-                <span className="ml-2 font-medium text-gray-500 dark:text-neutral-400">{item}</span>
-              </>
-            ),
-          },
-        ];
-      }
+export const PRODUCTS_MENU_LIST = Object.fromEntries(
+  (
+    ['The GraphQL Stack', ...FOUR_MAIN_PRODUCTS, 'Libraries', ...SIX_HIGHLIGHTED_PRODUCTS] as const
+  ).map((item, i) => {
+    if (typeof item === 'string') {
       return [
         i,
         {
-          type: 'page',
-          href: item.href,
+          href: '#',
           title: (
-            <div className="flex items-center gap-2">
-              <div
-                className={cn(
-                  'flex translate-y-[0.25px]',
-                  i > 6 && 'rounded-sm bg-gray-500 text-white dark:bg-white/10',
-                )}
-              >
-                <item.logo className="size-4 text-[8px]" />
-              </div>
-              {item.name}
-            </div>
+            <>
+              {/* This is a one-off class. The margins and paddings of the parent list item are were large. */}
+              {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
+              <style className="hive-label-separator">
+                {'li:has(>.hive-label-separator) { margin: 0.75rem 0 0.25rem 0; padding: 0 }'}
+              </style>
+              <span className="ml-2 font-medium text-gray-500 dark:text-neutral-400">{item}</span>
+            </>
           ),
         },
       ];
-    }),
-  );
+    }
+    return [
+      i,
+      {
+        href: item.href,
+        title: (
+          <div className="flex items-center gap-2">
+            <div
+              className={cn(
+                'flex translate-y-[0.25px]',
+                i > 6 && 'rounded-sm bg-gray-500 text-white dark:bg-white/10',
+              )}
+            >
+              <item.logo className="size-4 text-[8px]" />
+            </div>
+            {item.name}
+          </div>
+        ),
+      },
+    ];
+  }),
+);
