74dfafbaf51209a5b74c2569c43f037eefd11889
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-12-16 19:55:39 +0100

    Export sharedMetaItems for meta.ts files

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -20,6 +20,7 @@ export * from './types/components';
 export * from './logos';
 export { cn } from './cn';
 export * from './next-types';
+export { sharedMetaItems } from './shared-meta-items';
 
 declare module 'react' {
   interface CSSProperties {
---
+++ packages/components/src/shared-meta-items.ts
@@ -0,0 +1,47 @@
+import { PRODUCTS_MENU_LIST, ProductType } from './products';
+
+export function sharedMetaItems(options: { githubUrl: string; product: ProductType }) {
+  return {
+    products: {
+      title: 'Products',
+      type: 'menu',
+      items: PRODUCTS_MENU_LIST,
+    },
+    ecosystem: {
+      title: 'Ecosystem',
+      type: 'page',
+      ...(options.product === 'HIVE'
+        ? {}
+        : { href: 'https://the-guild.dev/graphql/hive/ecosystem' }),
+    },
+    blog: {
+      title: 'Blog',
+      type: 'page',
+      href: 'https://the-guild.dev/blog',
+    },
+    github: {
+      title: 'GitHub',
+      type: 'page',
+      href: options.githubUrl,
+    },
+    'the-guild': {
+      title: 'The Guild',
+      type: 'menu',
+      items: {
+        'about-us': {
+          title: 'About Us',
+          href: 'https://the-guild.dev/about-us',
+        },
+        'brand-assets': {
+          title: 'Brand Assets',
+          href: 'https://the-guild.dev/logos',
+        },
+      },
+    },
+    'graphql-foundation': {
+      title: 'GraphQL Foundation',
+      type: 'page',
+      href: 'https://graphql.org/community/foundation/',
+    },
+  };
+}
