37f0708536862c8fb02e6f398680146cdea1cca1
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-02-14 19:22:41 +0100

    Update failure thresholds in image snapshots

--- packages/og-image/src/handler.test.ts
+++ packages/og-image/src/handler.test.ts
@@ -9,25 +9,36 @@ vi.mock('../vender/index_bg.wasm', async () => {
 });
 
 describe('handler()', () => {
-  it('should works', async () => {
+  it('should work', async () => {
     const response = await handler({
       url: 'http://localhost:3000?product=CONDUCTOR',
     } as Request);
     const result = Buffer.from(await response.arrayBuffer());
-    expect(result).toMatchImageSnapshot();
+    expect(result).toMatchImageSnapshot({
+      failureThresholdType: 'percent',
+      failureThreshold: 0.2,
+    });
   });
+
   it('should align title and have container padding', async () => {
     const response = await handler({
       url: 'http://localhost:3000?product=ESLINT&title=Hello this is a test of really really really really really really long title',
     } as Request);
     const result = Buffer.from(await response.arrayBuffer());
-    expect(result).toMatchImageSnapshot();
+    expect(result).toMatchImageSnapshot({
+      failureThresholdType: 'percent',
+      failureThreshold: 0.2,
+    });
   });
+
   it('should align title without whitespaces', async () => {
     const response = await handler({
       url: 'http://localhost:3000?product=FETS&title=Home',
     } as Request);
     const result = Buffer.from(await response.arrayBuffer());
-    expect(result).toMatchImageSnapshot();
+    expect(result).toMatchImageSnapshot({
+      failureThresholdType: 'percent',
+      failureThreshold: 0.2,
+    });
   });
 });
